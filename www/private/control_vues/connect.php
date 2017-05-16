99023f50145bf2b3538a24ef24c25d5b4cb61c2a
L. Louis <louis.lemetayer35300@gmail.com>
2017-05-16 17:00:48 +0200

    formulaire de connexion ok... reste la gestion des inscription et surtout du traitement pour la connexion

--- www/private/control_vues/connect.php
+++ www/private/control_vues/connect.php
@@ -2,29 +2,18 @@
 <html>
 <?php
 // HEAD
-$title_head = "Accueil - SPSCT";
+$title_head = "SPSCT - connexion";
 include("private/vues/head/std.php");
 ?>
 <body>
-<?php
-// HEADER
-$title_header = "SPSCT";
-$page_actu = "accueil";
-include("private/vues/header/std.php");
-?>
 <section>
 	<?php
 	// ARTICLE TYPE 1
 	?>
-	<article id="type_2-1">
+	<article id="type_1">
 	<?php
-	include("private/vues/Accueil/art_1.php");
+	include("private/vues/connexion/connect.php");
 	?>
-	</article>
-	<article id="type_2-2">
-		<?php
-		include("private/vues/Menu/Action.php");
-		?>
 	</article>
 </section>
 </body>
---
+++ www/private/vues/connexion/connect.php
@@ -0,0 +1,6 @@
+<form id="champ_de_connexion">
+	<input type="text" name="user" />
+	<label for="user">Identifiant :  </label>
+	<input type="password" name="pass" />
+	<label for="pass">Mot de passe : </label>
+</form>
