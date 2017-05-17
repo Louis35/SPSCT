bff3cb30f1e3ca6cb6bf31347fe1338a18660c70
L. Louis <louis.lemetayer35300@gmail.com>
2017-05-17 20:22:59 +0200

    inclusion de la page de connexion si l'user non connecter dans index

--- www/index.php
+++ www/index.php
@@ -5,60 +5,68 @@ session_start();
 $bdd = new PDO('mysql:host=localhost;dbname=spsct','root', '');
 // $bdd variable de la base de donne a utiliser sur tout le site
 include("private/include.php");
-if (recup_day() == 1) 
+if(isset($_SESSION['Id']))
 {
-	reset_cumul_travail();
-}
-if(isset($_GET['pageId']))
-{
-	switch ($_GET['pageId']) 
+	if (recup_day() == 1) 
 	{
-		#Suivi Perf Scolaire
-		case 'sps':
-			include_once("private/control/sps/sps.php");
-			break;
-
-		#Calcule de Charge de Traille
-		case 'cct':
-			include_once("private/control/cct/cct.php");
-			break;
-
-		case 'setting':
-			include_once("private/control/setting/setting.php");
-			break;
-
-		case 'control':
-			include_once("private/control/control/control.php");
-			break;
-
-		default:
-			#ACCUEIL
-			include_once("private/control/accueil/accueil.php");
-			break;
+		reset_cumul_travail();
 	}
-}
-elseif (isset($_GET['action']))
-{
-	switch ($_GET['action']) 
+	if(isset($_GET['pageId']))
 	{
-		#ajout note
-		case 'post_grade':
-			include_once("private/control/post_grade/post_grade.php");
-			break;
+		switch ($_GET['pageId']) 
+		{
+			#Suivi Perf Scolaire
+			case 'sps':
+				include_once("private/control/sps/sps.php");
+				break;
 
-		case 'new_matiere':
-			include_once("private/control/new_matiere/new_matiere.php");
-			break;
-			
-		default:
-			#ACCUEIL
-			include_once("private/control/accueil/accueil.php");
-			break;
+			#Calcule de Charge de Traille
+			case 'cct':
+				include_once("private/control/cct/cct.php");
+				break;
+
+			case 'setting':
+				include_once("private/control/setting/setting.php");
+				break;
+
+			case 'control':
+				include_once("private/control/control/control.php");
+				break;
+
+			default:
+				#ACCUEIL
+				include_once("private/control/accueil/accueil.php");
+				break;
+		}
+	}
+	elseif (isset($_GET['action']))
+	{
+		switch ($_GET['action']) 
+		{
+			#ajout note
+			case 'post_grade':
+				include_once("private/control/post_grade/post_grade.php");
+				break;
+
+			case 'new_matiere':
+				include_once("private/control/new_matiere/new_matiere.php");
+				break;
+				
+			default:
+				#ACCUEIL
+				include_once("private/control/accueil/accueil.php");
+				break;
+		}
+	}
+	else
+	{
+		#ACCUEIL
+		include_once("private/control/accueil/accueil.php");
 	}
 }
 else
 {
-	#ACCUEIL
-	include_once("private/control/accueil/accueil.php");
+	include("private/control/connexion/connexion.php");
 }
+
 ?>
