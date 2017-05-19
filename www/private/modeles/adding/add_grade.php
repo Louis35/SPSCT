0a958d3825362c2c61d3221af4720953eda0893c
L. Louis <louis.lemetayer35300@gmail.com>
2017-05-19 13:19:21 +0200

    add_grade adapter

--- www/private/modeles/adding/add_grade.php
+++ www/private/modeles/adding/add_grade.php
@@ -8,12 +8,13 @@ function add_grade($trimestre, $Id_matiere, $Id_User $note, $coeff)
  	$note = (float) $note;
  	$coeff = (float) $coeff;
 
- 	$req = $bdd->prepare('INSERT INTO notes (`Trimestre`, `Id_matiere`, `Note`, `Coeff`) 
- 						  VALUES (:trimestre , :Id_matiere , :note , :coeff )');
+ 	$req = $bdd->prepare('INSERT INTO notes (`Trimestre`, `Id_matiere`, `Id_User` , `Note`, `Coeff`) 
+ 						  VALUES (:trimestre , :Id_matiere , :Id_user , :note , :coeff )');
  	$req->bindParam(':trimestre', $trimestre, PDO::PARAM_INT);
  	$req->bindParam(':Id_matiere', $Id_matiere, PDO::PARAM_INT);
  	$req->bindParam(':note', $note, PDO::PARAM_INT);
  	$req->bindParam(':coeff', $coeff, PDO::PARAM_INT);
+ 	$req->bindParam(':Id_user', $coeff, PDO::PARAM_INT);
 	$matieres = $req->execute();
 	$req->closeCursor();
  }
