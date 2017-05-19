983b3c6f94ecbbeb65d661bc6d98cbaa46b2db3a
L. Louis <louis.lemetayer35300@gmail.com>
2017-05-19 15:43:30 +0200

    recup all matiere + coeff adapter

--- www/private/modeles/getting/recup_all_matiere.php
+++ www/private/modeles/getting/recup_all_matiere.php
@@ -1,13 +1,14 @@
 <?php
-function recup_all_matieres($trimestre)
+function recup_all_matieres($trimestre, $Id_user)
  {
  	global $bdd;
 
  	$trimestre = (int) $trimestre;
 
  	$req = $bdd->prepare('SELECT Id, Matieres FROM matieres 
- 						  WHERE Trimestre = :Trimestre');
+ 						  WHERE Trimestre = :Trimestre AND Id_User = :Id_user');
  	$req->bindParam(':Trimestre', $trimestre, PDO::PARAM_INT);
+ 	$req->bindParam(':Id_user', $Id_user, PDO::PARAM_INT);
  	$req->execute();
 	$matieres = $req->fetchAll();
 	$req->closeCursor();
--- www/private/modeles/getting/recup_coeff_matiere.php
+++ www/private/modeles/getting/recup_coeff_matiere.php
@@ -1,5 +1,5 @@
 <?php
-function recup_coeff_matiere($Id_matiere)
+function recup_coeff_matiere($Id_matiere, $Id_user)
  {
  	global $bdd;
 
@@ -7,8 +7,9 @@ function recup_coeff_matiere($Id_matiere)
 
  	$req = $bdd->prepare('SELECT Coeff
  						  FROM matieres
- 						  WHERE Id = :Id_matiere');
+ 						  WHERE Id = :Id_matiere AND Id_User = :Id_user');
  	$req->bindParam(':Id_matiere', $Id_matiere, PDO::PARAM_INT);
+ 	$req->bindParam(':Id_user', $Id_user, PDO::PARAM_INT);
 	$req->execute();
 	$reponse = $req->fetch();
 	return $reponse['0'];
