2e7b8a52b90e48cc2e26a5abcb30bdfa60dbb4e4
L. Louis <louis.lemetayer35300@gmail.com>
2017-05-19 16:25:26 +0200

    calcule_temps_de_travail adapter

--- www/private/function/calcule_tempDeTravail.php
+++ www/private/function/calcule_tempDeTravail.php
@@ -1,11 +1,11 @@
 <?php
-function calcule_tempDeTravail($trimestre)
+function calcule_tempDeTravail($trimestre, $Id_user)
 {
-	$objectif = recup_objectif_moyenne($trimestre);
-	$pr = $objectif - round(recup_moyenne_trimestre($trimestre), 1);
+	$objectif = recup_objectif_moyenne($trimestre, $Id_user);
+	$pr = $objectif - round(recup_moyenne_trimestre($trimestre, $Id_user), 1);
 	$travailParJours = $pr * 20;
 	$travailParSemaine = $travailParJours * 7;
-	$cumul = recup_cumul();
+	$cumul = recup_cumul($Id_user);
 	$TravailRestant = $travailParSemaine - $cumul;
 	$travailDuJour = $TravailRestant / nombre_jours_restant_lundi();
 	$travailDuJour = round($travailDuJour, 0);
